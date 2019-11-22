ef29bd573997e17e77a597a366287e61df0e6807
rusu55 <rusu55@yahoo.com>
2019-11-21 21:38:41 -0600

    revolution slider added

--- public/js/custom.js
+++ public/js/custom.js
@@ -39,7 +39,7 @@ jQuery($ => {
       let $scroll = $(window).scrollTop();
       let $navbar = $(".static-nav");
 
-      if ($scroll > 250) {
+      if ($scroll > 150) {
         $navbar.addClass("fixedmenu");
       } else {
         $navbar.removeClass("fixedmenu");
@@ -83,3 +83,77 @@ jQuery($ => {
     offset: 0
   });
 });
+
+/* -- Revolution Slider -- */
+//creative agency index
+$("#rev_creative")
+  .show()
+  .revolution({
+    sliderType: "standard",
+    sliderLayout: "auto",
+    scrollbarDrag: "true",
+    dottedOverlay: "none",
+    navigation: {
+      keyboardNavigation: "off",
+      keyboard_direction: "horizontal",
+      mouseScrollNavigation: "off",
+      arrows: {
+        enable: false,
+        hide_onmobile: true,
+        hide_onleave: false,
+        hide_under: 767,
+        left: {
+          h_align: "left",
+          v_align: "center",
+          h_offset: 20,
+          v_offset: 30
+        },
+        right: {
+          h_align: "right",
+          v_align: "center",
+          h_offset: 20,
+          v_offset: 30
+        }
+      },
+      touch: {
+        touchenabled: "on",
+        swipe_threshold: 75,
+        swipe_min_touches: 1,
+        swipe_direction: "horizontal",
+        drag_block_vertical: false
+      }
+    },
+    viewPort: {
+      enable: true,
+      outof: "pause",
+      visible_area: "90%"
+    },
+    responsiveLevels: [4096, 1024, 778, 480],
+    gridwidth: [1140, 1024, 750, 480],
+    gridheight: [600, 500, 500, 350],
+    lazyType: "none",
+    parallax: {
+      type: "mouse",
+      origo: "slidercenter",
+      speed: 9000,
+      levels: [2, 3, 4, 5, 6, 7, 12, 16, 10, 50]
+    },
+    shadow: 0,
+    spinner: "off",
+    stopLoop: "off",
+    stopAfterLoops: -1,
+    stopAtSlide: -1,
+    shuffle: "off",
+    autoHeight: "off",
+    hideThumbsOnMobile: "off",
+    hideSliderAtLimit: 0,
+    hideCaptionAtLimit: 0,
+    disableProgressBar: "off",
+    hideAllCaptionAtLilmit: 0,
+    debugMode: false,
+    fallbacks: {
+      simplifyAll: "off",
+      nextSlideOnWindowFocus: "off",
+      disableFocusListener: false
+    }
+  });
